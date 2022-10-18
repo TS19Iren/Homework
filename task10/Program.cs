e9400167024ad26ca20083e7b5b944142faf5d7f
«Iren» <guzlik_iris@mail.ru>
2022-10-18 21:34:40 +0300

    commit

---
+++ task10/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу которая принимает на вход трехначное число и на выходе показывает вторую цифру этого числа
+Console.Write("Введите трехначное число: ");
+int consoleNumber = int.Parse(Console.ReadLine());
+if(consoleNumber>999||consoleNumber<100){
+    Console.Write("Введите трехзначное число!");
+}
+else {
+    int secondDigit = findSecondDigit(consoleNumber);
+    Console.Write($"Результат: {secondDigit}");
+}
+
+int findSecondDigit (int consoleNumber)
+    {
+        return (consoleNumber/10)%10;
+    }
