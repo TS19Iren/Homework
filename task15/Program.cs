faa17207e9a3ab945755ed2bfb6c6d7e2556dcd1
«Iren» <guzlik_iris@mail.ru>
2022-10-18 22:11:54 +0300

    commit

---
+++ task15/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 15: Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет*/
+Console.Write("Введите цифру: ");
+int number = int.Parse(Console.ReadLine());
+isWeekend(number);
+void isWeekend(int number)
+    {
+        if (number==6||number==7)
+        {
+            Console.Write("да");
+        }
+        else
+        {
+            Console.Write("нет");
+        }
+    }
