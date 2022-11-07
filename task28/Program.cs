fabb584d5c39de70d01890bfc61c8c6852850224
«Iren» <guzlik_iris@mail.ru>
2022-11-07 12:19:55 +0300

    task28

---
+++ task28/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задача 28: Напишите программу, которая
+//принимает на вход число N и выдаёт
+//произведение чисел от 1 до N.
+//4 -> 24
+//5 -> 120
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number>0)
+    {
+    int multNumbers = MultiNumbers(number);
+    Console.WriteLine($"Произведение = {multNumbers}");
+    }
+
+else
+    {
+        Console.Write("Вы ввели некорректное число");
+    }
+
+int MultiNumbers(int multiNum)
+    {
+        int multi = 1;
+
+        for (int i = 1; i <=multiNum; i++)
+        {
+            multi = multi*i;
+        }
+        return multi;
+    }
+    
