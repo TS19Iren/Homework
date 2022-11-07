dd8c816c2c8222a8b838a56d4816bb19e7e7d3e4
«Iren» <guzlik_iris@mail.ru>
2022-11-07 12:01:41 +0300

    task26

---
+++ task26/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу которая
+//принимает на вход число
+//выдает количество цифр в числе
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int SumDigit(int number)
+{
+    number = Math.Abs(number);
+    int count = default;
+    
+   while (number>0)
+   {
+    number=number/10;
+    count++;
+   }
+   return count>0? count:1;
+        
+}
+int sumDigit = SumDigit(num);
+Console.Write($"Количество цифр в числе  = {sumDigit}");
