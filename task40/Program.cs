3846855c0fe30d9dfb08ff36e96db4b6a460eff9
«Iren» <guzlik_iris@mail.ru>
2022-11-14 10:52:03 +0300

    task40

---
+++ task40/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 40: Напишите программу, которая 
+принимает на вход три числа и 
+проверяет, может ли существовать треугольник с сторонами
+такой длины.
+Теорема о неравенстве треугольника: каждая сторона треугольника
+меньше суммы двух других сторон.*/
+
+Console.WriteLine("Введите три числа: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+bool CheckSides(int num1,int num2,int num3)
+
+{
+       
+        bool b = (num1+num2>num3&&num2+num3>num1&&num1+num3>num2);
+        return b;
+}
+
+bool result = CheckSides(num1,num2,num3);   
+Console.WriteLine((result?"такой треугольник существует":"такого треугольника быть не может"));
