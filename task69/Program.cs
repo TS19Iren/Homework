79dcbfa98c8f0541d77cf9e7d4631df8b4cf5987
«Iren» <guzlik_iris@mail.ru>
2022-11-24 11:50:30 +0300

    task69

---
+++ task69/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 69: Напишите программу, которая на вход принимает два числа A и B, и
+возводит число А в целую степень B с помощью рекурсии.
+A = 3; B = 5 -> 243 (3⁵)
+A = 2; B = 3 -> 8*/
+
+Console.WriteLine("Enter first number A: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter first number B: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+int Involution(int a, int b)
+{
+    
+    int inv = a;
+    if(b==1) return inv;
+    else
+    {
+        inv = a*Involution(a,b-1);
+        return inv;
+    }
+}
+
+int involution = Involution(numA,numB);
+Console.WriteLine($"Число А в степени В: {involution}");
